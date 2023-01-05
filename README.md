1b4f2447b435741ab358fd20795ef875f427dadc
yhArcadia <1761869682@qq.com>
2023-01-05 16:22:34 +0800

    modified:   apps/remove_bg.js

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
  * @Author: 渔火Arcadia  https://github.com/yhArcadia
  * @Date: 2022-12-22 00:38:21
  * @LastEditors: 渔火Arcadia
- * @LastEditTime: 2023-01-05 16:15:39
+ * @LastEditTime: 2023-01-05 16:22:30
  * @FilePath: \Yunzai-Bot\plugins\ap-plugin\README.md
- * @Description: 版本：2.6.2
+ * @Description: 版本：2.6.3
  *
  * Copyright (c) 2022 by 渔火Arcadia 1761869682@qq.com, All Rights Reserved.
 -->
--- apps/remove_bg.js
+++ apps/remove_bg.js
@@ -2,7 +2,7 @@
  * @Author: 苏苏
  * @Date: 2023-01-04 19:44:45
  * @LastEditors: 渔火Arcadia
- * @LastEditTime: 2023-01-04 20:17:19
+ * @LastEditTime: 2023-01-05 16:20:55
  * @FilePath: \Yunzai-Bot\plugins\ap-plugin\apps\remove_bg.js
  * @Description: 去除图片背景
  * 
@@ -16,7 +16,7 @@ import { segment } from 'oicq'
 
 const _path = process.cwd();
 let ap_cfg = await Config.getcfg()
-const URL = ap_cfg.remove_bg 
+const URL = ap_cfg.remove_bg
 let FiguretypeUser = {}
 let getImagetime = {}
 
@@ -51,12 +51,12 @@ export class RemoveBackground extends plugin {
 
     async AnimeRemoveBackground(e) {
         if (!URL)
-        return await e.reply("请先配置去背景所需api，配置教程：https://www.wolai.com/sSZM1AHnBULxyc4s4hKquF")
+            return await e.reply("请先配置去背景所需api，配置教程：https://www.wolai.com/sSZM1AHnBULxyc4s4hKquF")
         //将URL处理成API格式
         const API = 'https://' + URL.split('/')[4] + '-anime-remove-background.hf.space/api/queue/'
 
         if (FiguretypeUser[e.user_id]) {
-            e.reply('当前有任务在列表中排队，请不要重复发送，去背景完成后会自动发送结果，如果长时间没有结果，请等待1分钟再试')
+            e.reply('当前有任务在列表中排队，请不要重复发送，去背景完成后会自动发送结果，如果长时间没有结果，请等待1分钟再试', false, { at: true, recallMsg: 15 })
             return true
         }
         if (e.source) {
@@ -78,7 +78,7 @@ export class RemoveBackground extends plugin {
             }
         }
         if (this.e.img) {
-            e.reply('正在为图像去背景，请稍后...', true)
+            e.reply('正在为图像去背景，请稍候...', false, { at: true, recallMsg: 15 })
             FiguretypeUser[e.user_id] = setTimeout(() => {
                 if (FiguretypeUser[e.user_id]) {
                     delete FiguretypeUser[e.user_id];
@@ -125,10 +125,10 @@ export class RemoveBackground extends plugin {
             console.log(`本次请求完成`)
             let end = new Date()
             let time = ((end.getTime() - start.getTime()) / 1000).toFixed(2)
-            e.reply(`耗时${time}s，正在发送结果...`, true)
+            e.reply(`耗时${time}s，正在发送结果...`, false, { at: true, recallMsg: 5 })
             res.data.data.data[1] = res.data.data.data[1].replace(/^data:image\/png;base64,/, "")
             let buffer = Buffer.from(res.data.data.data[1], 'base64')
-            await e.reply(segment.image(buffer))
+            await e.reply(segment.image(buffer), true)
             delete FiguretypeUser[e.user_id];
             return true
         } else {
